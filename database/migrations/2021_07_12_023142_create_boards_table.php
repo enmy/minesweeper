c2d8b7a8f857437e38f52b02e933ef9a40a47c3b
enmy perez <enmyperez@gmail.com>
2021-07-11 22:44:50 -0400

    feat: create board model

---
+++ database/migrations/2021_07_12_023142_create_boards_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBoardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            $table->id();
+            $table->json('board');
+            $table->json('dimensions');
+            $table->integer('seconds');
+            $table->string('browser_id')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boards');
+    }
+}
